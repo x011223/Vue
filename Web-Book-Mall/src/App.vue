4d4e21758210e573db6b51c93ecb42a01c9e27a5
Feafortecsy <31724313+Feafortecsy@users.noreply.github.com>
2017-11-02 22:36:59 +0800

    2017-11-02
    Web-Book-Mall Created

---
+++ Web-Book-Mall/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bodt">
+    Hello Everybody
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+  .bodt {
+    text-align: center;
+    color: #EC173E;
+  }
+</style>
