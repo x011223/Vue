9d048634a7c62073e5844b9fb863c186cf715dbe
Feafortecsy <1228256157@qq.com>
2017-11-30 23:02:28 +0800

    2017-11-30
    song-list组件样式重写

--- Fashion-Music/src/base/song-list/song-list.vue
+++ Fashion-Music/src/base/song-list/song-list.vue
@@ -2,19 +2,16 @@
     <div class="song-list">
         <ul>
             <li @click="selectSong(song, index)" v-for="(song, index) in songs" class="song">
-                <div class="song-content">
-                    <div class="rankcss iconfont" v-show="rankStyle">
-                        <span :class="getRankStyle(index)">{{getRankText(index)}}</span>
-                    </div>
-                    <div class="song-image">
-                        <img v-lazy="song.image" width="36" height="36">
-                    </div>                   
-                    <div class="song-content">
-                        <h2 class="song-name">{{song.name}}</h2>
-                        <p class="song-desc">{{getSongDesc(song)}}</p>
-                    </div> 
-                    <img src="./song.svg" class="icon-song" width="32" height="32">                 
+                <div class="rankcss iconfont" v-show="rankStyle">
+                    <span :class="getRankStyle(index)">{{getRankText(index)}}</span>
                 </div>
+                <div class="song-image">
+                    <img v-lazy="song.image" width="36" height="36">
+                </div>                   
+                <div class="song-text">
+                    <h2 class="song-name">{{song.name}}</h2>
+                    <p class="song-desc">{{getSongDesc(song)}}</p>
+                </div>                
             </li>
         </ul>
     </div>
@@ -68,63 +65,65 @@
         background: $background-color-list;
         ul {
             .song {
+                height: $height-big-xx;
+                padding-top: 1rem;
                 .rankcss {
+                    display: inline-block;
+                    width: 25px;
+                    vertical-align: top;
+                    font-size: $font-size-x;
+                    margin-left: 2rem;
                 }
                 .song-image {
-                    padding-left: 1.2rem;
-                    margin-top: 0.8rem;
+                    padding-left: 1.5rem;
+                    display: inline-block;
                 }
-                .song-content {
+                .song-text {
+                    position: absolute;
+                    left: 12rem;
+                    margin-top: -2.7rem;               
+                    flex-direction: column;
                     display: flex;
-                    padding-left: 0.5rem;
-                    margin-top: 0.5rem;
-                    .rankcss {
-                        display: inline-block;
-                        width: 25px;
-                        text-align: center;
-                        font-size: $font-size-x;
-                        border-right: 2px solid #51a8ff;
-                        border-radius: 10px;
-                        margin-top: 0.5rem;
-                        padding: {
-                            top: 4px;
-                            right: 12px;
-                        }
-                    }
-                    .icon0, .icon1, .icon2 {
-                        font-size: 32px;
-                        margin-top: 0.5rem;
-                    }
-                    .icon0 {
-                        color: #ffd700;
-                    }
-                    .icon1 {
-                        color: #c0c0c0;
-                    }
-                    .icon2 {
-                        color: #ff331e;
-                    }
+                    margin-left: -3rem;
                     .song-name {
                         flex: 1;
-                        font-weight: $font-weight-s;
-                        font-size: $font-size-b;
+                        align-items: center;
+                        font: {
+                            size: $font-size-b;
+                            weight: $font-weight-s;
+                        }
                         color: $color-text-name;
+                        padding-bottom: 0.7rem;
+                        text-overflow: ellipsis;
+                        white-space: nowrap;
+                        overflow: hidden;
                         @include lineFlow($width: 100%);
                     }
                     .song-desc {
-                        position: absolute;
-                        padding-top: 1.9rem;
-                        font-weight: $font-weight-s;
-                        font-size: $font-size-n;
-                        color: $color-text-desc;
-                        @include lineFlow($width: 100%);
+                        font: {
+                            size: $font-size-n;
+                            weight: $font-weight-s;
+                            color: $color-text-desc;                                          
+                        }
+                        text-overflow: ellipsis;
+                        white-space: nowrap;
+                        overflow: hidden;
+                        @include lineFlow($width: 100%);             
                     }
+                    
                 }
-                .icon-song {
-                    position: absolute; 
-                    right: 20px;
-                    padding-left: 1.2rem;
-                    margin-top: 0.8rem;
+                .icon0, .icon1, .icon2 {
+                    font-size: 32px;
+                    margin-top: 0.5rem;
+                }
+                .icon0 {
+                    color: #ffd700;
+                }
+                .icon1 {
+                    color: #c0c0c0;
+                }
+                .icon2 {
+                    color: #ff331e;
                 }
             }
         }
@@ -136,47 +135,62 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    /* 
-    .icon {
-        display: inline-block;
-        width: 25px;
-        height: 24px;
-        background-size: 25px 24px;
-    }
-    .rankText {
-        color: #000080;
-        font-size: 14px;
-        margin-top: 8px;
-    }
-    .song {
-        display: flex;
-        align-items: center;
-        box-sizing: border-box;
-        height: 64px;
-        top: 8px;
-        overflow: hidden;
-        background: rgba(255, 255, 255, 0.3);
-    }
-    
-    .song-content {
-        flex: 1;
-    }
-    .song-image {        
-        display: inline-block;
-        vertical-align: top;
-        margin-left: 12px;
-    }
- */
+    // .song-list {
+    //     width: 100%;
+    //     background: $background-color-list;
+    //     ul {
+    //         .song {
+    //             .song-image {
+    //                 padding-left: 1.2rem;
+    //                 margin-top: 0.8rem;
+    //             }
+    //             .song-content {
+    //                 display: flex;
+    //                 padding-left: 0.5rem;
+    //                 margin-top: 0.5rem;
+    //                 .rankcss {
+    //                     display: inline-block;
+    //                     width: 25px;
+    //                     text-align: center;
+    //                     font-size: $font-size-x;
+    //                     border-right: 2px solid #51a8ff;
+    //                     border-radius: 10px;
+    //                     margin-top: 0.5rem;
+    //                     padding: {
+    //                         top: 4px;
+    //                         right: 12px;
+    //                     }
+    //                 }
+    //                 .icon0, .icon1, .icon2 {
+    //                     font-size: 32px;
+    //                     margin-top: 0.5rem;
+    //                 }
+    //                 .icon0 {
+    //                     color: #ffd700;
+    //                 }
+    //                 .icon1 {
+    //                     color: #c0c0c0;
+    //                 }
+    //                 .icon2 {
+    //                     color: #ff331e;
+    //                 }
+    //                 .song-name {
+    //                     flex: 1;
+    //                     font-weight: $font-weight-s;
+    //                     font-size: $font-size-b;
+    //                     color: $color-text-name;
+    //                     @include lineFlow($width: 100%);
+    //                 }
+    //                 .song-desc {
+    //                     position: absolute;
+    //                     padding-top: 1.9rem;
+    //                     font-weight: $font-weight-s;
+    //                     font-size: $font-size-n;
+    //                     color: $color-text-desc;
+    //                     @include lineFlow($width: 100%);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 </style>
