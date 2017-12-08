58568388c3d39eaf8811e8a156304140c57e67e8
Feafortecsy <1228256157@qq.com>
2017-12-08 23:11:17 +0800

    2017-12-08
    开始模仿饿了么项目

---
+++ eleme/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <header-wrapper></header-wrapper>
+    </div>
+</template>
+
+<script>
+    import HeaderWrapper from './components/header/headerwrapper'
+    export default {
+        name: 'app',
+        components: {
+            HeaderWrapper,
+        }
+    }
+</script>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+</style>
